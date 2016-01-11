4f322d5c158967d7de83bc639268dfbf9f6264f7
lucgilr <luciagilroman@gmail.com>
2016-01-11 19:26:59 +0100

    Creada función para obtener predecesoras

--- src/java/Data/PredecesorasDB.java
+++ src/java/Data/PredecesorasDB.java
@@ -8,7 +8,10 @@ package Data;
 import Business.Actividad;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
@@ -31,6 +34,28 @@ public class PredecesorasDB {
         }
     }
      
-    
+    public static List<Integer> obtenerPredecesoras(int id){
+        ConnectionPool pool = ConnectionPool.getInstance();
+        Connection connection = pool.getConnection();
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        String query = "SELECT * FROM Predecesoras WHERE actividad=?";
+        List<Integer> predecesoras = new ArrayList<Integer>();
+         try {
+            ps = connection.prepareStatement(query);
+            ps.setInt(1, id);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                predecesoras.add(rs.getInt("predecesora"));
+            }
+            rs.close();
+            ps.close();
+            pool.freeConnection(connection);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return predecesoras;
+        
+    }
     
 }
